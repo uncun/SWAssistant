a26bb8dc3d4585fcc03bdf3040b75274964a2ff3
uncun <48922026+uncun@users.noreply.github.com>
2024-08-24 14:42:28 +0200

    test

--- uncodedeeee.js
+++ uncodedeeee.js
@@ -563,6 +563,7 @@ if (typeof GAME === 'undefined') {} else {
                 wait_wymiana: 600,
                 cap_wymiana: 0,
                 res_id: 0,
+                exchange_id: 0,
             };
             INNE.start = () => {
                 if (INNE.wymiana && !GAME.is_loading) {
@@ -574,6 +575,25 @@ if (typeof GAME === 'undefined') {} else {
 
             INNE.action = () => {
                 INNE.cap_wymiana = $("#inne_Panel input[name=insta_capt]").val();
+                if (INNE.ronin) {
+                    INNE.res_id = document.querySelector('select[name="ronin_opt"]').value;
+                    INNE.exchange_id = 9;
+                }
+                if (INNE.karciana) {
+                    INNE.res_id = document.querySelector('select[name="karciana_opt"]').value;
+                    INNE.exchange_id = 13;
+                }
+
+                window.inter_wymiana = window.setInterval(function() {
+                    if (INNE.cap_wymiana > 0) {
+                        console.log("wymiana", INNE.cap_wymiana, INNE.res_id, INNE.exchange_id);
+                    } else if (INNE.cap_wymiana <= 0) {
+                        console.log("koniec wymiany");
+                        INNE.wymiana = false;
+                        $(".inne_wymiana .inne_status").removeClass("green").addClass("red").html("Off");
+                    }
+                    INNE.cap_wymiana--;
+                }, 100);
             };
 
             var PVP = {
