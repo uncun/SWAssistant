6833eed67c74008ff35be14d982ac4ea59773061
uncun <48922026+uncun@users.noreply.github.com>
2024-08-25 13:09:48 +0200

    test

--- script1-2.js
+++ script1-2.js
@@ -1037,7 +1037,7 @@ if (typeof GAME === 'undefined' && extrapremium) { } else {
                                         if (this.item_jakosc_cap > res.item.quality) {
                                             console.log("jakosc git")
                                             this.item_jakosc = false;
-                                            $(".item_poziom .item_status").removeClass("green").addClass("red").html("Off");
+                                            $(".item_jakosc .item_status").removeClass("green").addClass("red").html("Off");
                                         } else {
                                             window.setTimeout(this.UpgradeItem,100);
                                         }
@@ -1047,7 +1047,7 @@ if (typeof GAME === 'undefined' && extrapremium) { } else {
                                         if (this.item_poziom_cap > res.item.upgrade) {
                                             console.log("poziom git")
                                             $(".item_poziom .item_jakosc").removeClass("green").addClass("red").html("Off");
-                                            this.item_jakosc = false;
+                                            this.poziom = false;
                                         } else {
                                             window.setTimeout(this.RerollItem,100);
                                         }
@@ -1126,7 +1126,7 @@ if (typeof GAME === 'undefined' && extrapremium) { } else {
                         $(".item_jakosc .item_status").removeClass("green").addClass("red").html("Off");
                         this.item_jakosc = false;
                         this.item_poziom = true;
-                        this.item_jakosc_cap = $("#ItemPanel input[name=jakosc_capt]").val();
+                        this.item_poziom_cap = $("#ItemPanel input[name=jakosc_capt]").val();
                         window.setTimeout(this.UpgradeItem,100);
  
                     } else {
