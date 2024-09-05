375b612be7900c5879b2d5fc8daa2b4495b02976
uncun <48922026+uncun@users.noreply.github.com>
2024-09-05 21:48:39 +0200

    test

--- uncodedeeee.js
+++ uncodedeeee.js
@@ -1032,7 +1032,7 @@ if (typeof GAME === 'undefined') {} else {
                 console.log("PVP ", PVP.start_char, PVP.chars.length)
                 if (PVP.start_char == PVP.chars.length) {
                     PVP.start_char = 0;
-                    console.log("PVP ", PVP.start_char)
+                    console.log("PVP ", PVP.start_char, (PVP.pvp_timeout-GAME.getTime() * 1000))
                     var charId = parseInt(PVP.chars[0]);
                     GAME.emitOrder({ a: 2, char_id: charId });
                     window.setTimeout(PVP.start, (PVP.pvp_timeout-GAME.getTime() * 1000));
